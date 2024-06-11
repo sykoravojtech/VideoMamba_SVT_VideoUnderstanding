720a5aff6170ba7113c6d42cb5a89e89635428ba
Swadesh13 <swadeshjana@gmail.com>
2024-06-11 16:04:14 +0000

    Added basic caption model code

--- src/models/_factory.py
+++ src/models/_factory.py
@@ -1,12 +1,12 @@
 from fvcore.common.config import CfgNode
 
 from .classification_model import VideoClassificationModel
-from .captioning_model import VideoCaptioningnModel
+from .captioning_model import VideoCaptioningModel
 
 def create_model(config: CfgNode):
     model_type = config.MODEL.TYPE
     if(model_type == 'classification'):
         return VideoClassificationModel(config)
     if(model_type == 'captioning'):
-        return VideoCaptioningnModel(config)
+        return VideoCaptioningModel(config)
     raise NotImplementedError(f'{model_type} is not implemented.')
--- src/models/heads/_head_factory.py
+++ src/models/heads/_head_factory.py
@@ -1,7 +1,10 @@
 from .mlp_head import MLPHead
+from .gpt2 import load_language_model
 
 def create_head(config):
     head_type = config.MODEL.HEAD.TYPE
     if head_type == 'MLP':
         return MLPHead(config)
+    elif "gpt2" in head_type:
+        return load_language_model(head_type)
     raise ModuleNotFoundError(f'No head called: {head_type}')
